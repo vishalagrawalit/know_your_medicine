7d86953c2dadde438e536a963bf12da1313036cd
vishuvish <vishvishu14@gmail.com>
2018-04-01 14:41:12 +0530

    calling the function from medicine_details

--- crawler.py
+++ crawler.py
@@ -2,14 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 #Website containing medicine details
-url = "https://www.drugs.com/lexapro.html"
 
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-data = soup.find_all("p")
+def get_details(medicine_name):
+    url = "https://www.drugs.com/" + medicine_name + ".html"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    data = soup.find_all("p")
 
-temp = [] #List for storing text values from html
-for text in data:
-    temp.append(text.get_text())
-    
-print(temp[1:4])
+    temp = [] #List for storing text values from html
+    i = 0
+    for text in data:
+        temp.append(text.get_text())
+        
+        if i>=1 and i<5:
+            print(temp[i])
+        i+=1
