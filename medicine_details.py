955f93c314087436fa1d75c8285ad362e5994951
vishvishu <vishvishu14@gmail.com>
2018-04-01 14:15:24 +0530

    source path updated

--- medicine_details.py
+++ medicine_details.py
@@ -6,9 +6,10 @@ import cv2
 import os
 
 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
+src_path = "C:\git\know_your_medicine\Image"
 
 # load the example image and convert it to grayscale
-image = cv2.imread("C:/Users/VISHAL/images/" + med2.jpg)
+image = cv2.imread(src_path + med2.jpg)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow("Image", gray)
@@ -17,16 +18,16 @@ cv2.imshow("Image", gray)
 # apply OCR to it
 filename = "{}.png".format(os.getpid())
 print(filename)
-cv2.imwrite("C:/Users/VISHAL/images/" + filename, gray)
+cv2.imwrite(src_path + filename, gray)
 
+# show the saved gray image
 #img = cv2.imread("C:/Users/VISHAL/images/" + filename)
 #cv2.imshow(filename, img)
 
 # load the image as a PIL/Pillow image, apply OCR, and then delete
 # the temporary file
-src_path = r"C:/Users/VISHAL/images/"
 text = pytesseract.image_to_string(Image.open(src_path + filename))
-os.remove("C:/Users/VISHAL/images/" + filename)
+os.remove(src_path + filename)
 print(text)
 
 # show the output images
