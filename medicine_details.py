e628909dee945251de9e1610b5c5efc7c5ccf181
vishuvish <vishvishu14@gmail.com>
2018-04-01 14:43:10 +0530

    integrated crawler.py to show the details of the medicine

--- medicine_details.py
+++ medicine_details.py
@@ -4,12 +4,12 @@ import pytesseract
 import argparse
 import cv2
 import os
+from crawler import get_details
 
-pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
-src_path = "C:\git\know_your_medicine\Image"
+pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 
 # load the example image and convert it to grayscale
-image = cv2.imread(src_path + med2.jpg)
+image = cv2.imread("C:/git/know_your_medicine/images/" + "med2.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 cv2.imshow("Image", gray)
@@ -18,17 +18,17 @@ cv2.imshow("Image", gray)
 # apply OCR to it
 filename = "{}.png".format(os.getpid())
 print(filename)
-cv2.imwrite(src_path + filename, gray)
+cv2.imwrite("C:/git/know_your_medicine/images/" + filename, gray)
 
-# show the saved gray image
 #img = cv2.imread("C:/Users/VISHAL/images/" + filename)
 #cv2.imshow(filename, img)
 
 # load the image as a PIL/Pillow image, apply OCR, and then delete
 # the temporary file
+src_path = "C:/git/know_your_medicine/images/"
 text = pytesseract.image_to_string(Image.open(src_path + filename))
-os.remove(src_path + filename)
-print(text)
+os.remove("C:/git/know_your_medicine/images/" + filename)
+get_details(text)
 
 # show the output images
 # cv2.imshow("Image", image)
